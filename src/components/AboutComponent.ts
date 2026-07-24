1c8be29ba53655e707bf07bbe7772c09146e9685
elenamazyrina <mazyrina.mail@gmail.com>
2026-07-25 03:07:03 +0500

    product initialization (astro + angular)

---
+++ src/components/AboutComponent.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-about',
+    standalone: true,
+    template: `
+    <div>
+      Beat it!!!
+    </div>
+  `
+})
+export class AboutComponent {
+}
