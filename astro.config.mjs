9559cc9e4da8bf3aeed69619c699a69dd902fed1
elenamazyrina <mazyrina.mail@gmail.com>
2026-09-20 00:25:34 +0300

    change astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import analogjsangular from '@analogjs/astro-angular';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ElenaMazyrina.github.io',
-  base: 'My-Portfolio', //path to your site inside domain
+  base: '/My-Portfolio', //path to my site inside domain
   integrations: [analogjsangular({
     vite: {
       inlineStylesExtension: 'scss|sass|less'
